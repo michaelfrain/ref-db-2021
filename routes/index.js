74029ee76434840e1bdc9d9c3e4109675ae01f57
Michael Frain <michaelfrain@Michaels-iMac.local>
2022-06-30 09:40:40 -0700

    Update image sizes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SCIAC Home' });
+  res.render('index', { title: 'SCIAC Home', image: "images/sciac-logo.png" });
 });
 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -3,12 +3,11 @@ var router = express.Router();
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Login to the SCIAC Portal' });
+    res.render('login', { title: 'Login to the SCIAC Portal' });
 });
 
 router.post('/', function(req, res) {
-    console.log(req.body.username);
-    console.log(req.body.password);
+
 });
 
 module.exports = router;
