392f2f775e4c7be897b5ab4e6d325a58f309f19e
Michael Frain <michael.f.frain@disney.com>
2023-12-14 14:43:53 -0800

    Update models

--- models/game.js
+++ models/game.js
@@ -1,12 +1,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Team = require('./team');
-const User = require('./user')
+const User = require('./user');
+const Play = require('./play');
 
 const Game = new Schema({ 
+    gameNumber: Number,
+    date: Date,
     homeTeam: Team,
-    visitingTeam: Team,
-    gameDateTime: Date,
+    visitorTeam: Team,
+    homeScore: Number,
+    visitorScore: Number,
+    televised: {
+        type: Boolean,
+        default: false
+    },
+    startTime: Date,
+    regEndTime: Date,
+    otEndTime: Date,
+    overtime: {
+        type: Boolean,
+        default: false
+    },
+    numberOfOvertimes: {
+        type: Number,
+        default: 0
+    },
+    gradedPlays: [Play],
     officials: [User]
 });
 
--- models/play.js
+++ models/play.js
@@ -1,6 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Team = require('./team');
+const User = require('./user');
 
-const Foul = new Schema({ 
-    code: String
+const Play = new Schema({ 
+    entry: {
+        type: String,
+        enum: ["Foul", "Special", "Comment", "Feigning Injury"]
+    },
+    quarter: Number,
+    time: {
+        minutes: Number,
+        seconds: Number
+    },
+    playType: {
+        type: String,
+        enum: ["Dead Ball", "FG", "Free Kick", "None", "Pass", "Punt", "Run", "Try"]
+    },
+    playVideoNumber: Number,
+    foulCode: String,
+    category: String,
+    acceptDecline: String,
+    offenseDefense: String,
+    team: Team,
+    player: [Number],
+    officialsCalling: [User],
+    description: String,
+    grade: String,
+    comment: String
 });
